53f3933ec807426853e6de5d061b6d163a02cb25
wjforero <wilson.forero@uan.edu.co>
2020-09-19 15:02:03 -0500

    Adding default home page

--- app/simpleapi/urls.py
+++ app/simpleapi/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views import debug
 
 urlpatterns = [
+    path('', debug.default_urlconf),
     path('admin/', admin.site.urls),
     path('api/1.0/', include('generic.urls')),
 ]
