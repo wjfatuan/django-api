ad564ec50ee98b8ab01b4a770e1470256af98caa
wjforero <wilson.forero@uan.edu.co>
2020-09-25 18:39:40 -0500

    Fix to code style issues (codacy)

--- app/generic/serializers.py
+++ app/generic/serializers.py
@@ -6,6 +6,8 @@ from .models import Data
 class DataSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         """Serializer fields.
+
+        Defines the fields to show when the serializer is used.
         """
         model = Data
         fields = ['id', 'name', 'data']
