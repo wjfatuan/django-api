5a3c376b75a9803f973e8294aec164e282fb43ad
wjfatuan <wilson.forero@uan.edu.co>
2020-10-03 12:28:42 -0500

    Adding Django tests

--- app/generic/models.py
+++ app/generic/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-# Create your models here.
 class Data(models.Model):
     name = models.CharField('Name of the data set', max_length=256)
     data = models.JSONField('JSON Data')
+
+    def __str__(self) -> str:
+        return self.name
